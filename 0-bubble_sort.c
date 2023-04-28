5a07ab9ec0aa69adeeae75fb99ddda751e2293ad
esmond-adjei <xmongram@gmail.com>
2023-04-28 20:07:39 +0000

    modified some files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,34 +3,29 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort integers in an array
- * @array[]: array of integers
+ * bubble_sort - sort integer array
+ * @array: array of integeer
  * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, tail;
-	char is_sorted = 0;
-	int num;
+	size_t i, j;
+	int tmp;
 
 	if (size < 2)
 		return;
- 
-	tail = size;
-	while (!is_sorted)
+
+	for (i = 0; i < size; i++)
 	{
-		is_sorted = 1;
-		for (i = 0; i < tail - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				num = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = num;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
-				is_sorted = 0;
 			}
 		}
-		tail--;
 	}
 }
