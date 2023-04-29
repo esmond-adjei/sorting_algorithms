832c86a889deb10eb3d6d0d938035c2dc4b59185
esmond-adjei <xmongram@gmail.com>
2023-04-29 12:59:20 +0000

    modified some files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,39 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers using
- * selection sort algorithm
- * @array: array to be sorted
- * @size: size of the array
+ * swap - swaps two integers in an array
+ * @a: first number
+ * @b: second number
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - sort array using the selection sort algorithm
+ * @array: the array to be sorted
+ * @size:size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-	/**
-	 * pd: platform dancer
-	 * cg: challenger
-	 */
-	size_t pd, cg;
+	unsigned int i, j, tmp_j = 0;
+	int tmp;
 
-	for (pd = 0; pd < size; pd++)
+	for (i = 0; i < size; i++)
 	{
-		for (cg = pd + 1; cg < size; cg++)
+		tmp = array[i], tmp_j = i;
+		for (j = i; j < size; j++)
 		{
-			if (array[pd] > array[cg])
-			{
-				int tmp = array[cg];
-
-				array[cg] = array[pd];
-				array[pd] = tmp;
-			}
+			if (array[j] < tmp)
+				tmp = array[j], tmp_j = j;
+		}
+		if (array[i] != array[tmp_j])
+		{
+			swap(array + i, array + tmp_j);
+			print_array(array, size);
 		}
 	}
 }
