0f145db4ddab8ce4ddab3599e301007326aa7788
esmond-adjei <xmongram@gmail.com>
2023-04-29 11:51:29 +0000

    modified some files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,96 @@
 #include "sort.h"
 
 /**
- * swap_nodes - swaps nodes in a doubly linked list
- * @list: pointer to the head of the list
- * @node1: first node
- * @node2: second node
- */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+ * swap_left -swap two nodes right left position
+ * @list: list
+ *Return: NULL
+ *
+ **/
+void swap_left(listint_t *list)
 {
-	if (node1->prev != NULL)
-		node1->prev->next = node2;
-	else
-		*list = node2;
+	listint_t *tmp, *head;
 
-	if (node2->next != NULL)
-		node2->next->prev = node1;
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node2->next = node1;
-	node1->prev = node2;
-	print_list(*list);
+	while (list->prev != NULL)
+	{
+		if (list->n < list->prev->n)
+		{
+			tmp = list->prev->prev;
+			list->prev->next = list->next;
+			list->next = list->prev;
+			list->prev->prev = list;
+			list->prev = tmp;
+			list->next->next->prev = list->next;
+			if (tmp != NULL)
+				tmp->next = list;
+			head = list;
+			while (head->prev != NULL)
+				head = head->prev;
+			print_list(head);
+		}
+		else
+			list = list->prev;
+	}
 }
 
 /**
- * insertion_sort_list - sorts a doubly linked list in ascending order
- * @list: pointer to the head of the list
+ * swap_right -swap two nodes left rigth position
+ * @l: list
+ * Return: Null
+ **/
+void swap_right(listint_t *l)
+{
+	listint_t *tmp, *head;
+
+	tmp = l->prev;
+
+	if (tmp != NULL)
+	{
+		tmp->next = l->next;
+		l->next->prev = tmp;
+	}
+	else
+		l->next->prev = NULL;
+	l->prev = l->next;
+	if (l->next->next != NULL)
+	{
+		l->next = l->next->next;
+		l->prev->next = l;
+		l->next->prev = l;
+	}
+	else
+	{
+		l->next->next = l;
+		l->next = NULL;
+	}
+	head = l;
+	while (head->prev != NULL)
+		head = head->prev;
+	print_list(head);
+	swap_left(l->prev);
+}
+
+/**
+ * insertion_sort_list -sorts a doubly linked list in ascending order
+ * @list: list
+ * Return: NULL
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr = (*list)->next;
+	listint_t *l;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if ((list == NULL) || (*list == NULL) || ((*list)->next == NULL))
 		return;
+	l = *list;
 
-	while (curr != NULL)
+	while (l->next != NULL)
 	{
-		listint_t *prev = curr->prev;
-
-		while (prev != NULL && prev->n > curr->n)
+		if (l->n > l->next->n)
 		{
-			swap_nodes(list, prev, curr);
-			prev = curr->prev;
+			swap_right(l);
 		}
-		curr = curr->next;
+		else
+			l = l->next;
 	}
+	while ((*list)->prev != NULL)
+		*list = (*list)->prev;
 }
--- mine/1-insertion_sort_list.c
+++ mine/1-insertion_sort_list.c
@@ -1,34 +1,48 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * insertion_sort_list - sort doubly list data types using insertion sort
- * @list: doubly linked list data type
+ * swap_nodes - swaps nodes in a doubly linked list
+ * @list: pointer to the head of the list
+ * @node1: first node
+ * @node2: second node
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next != NULL)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node2->next = node1;
+	node1->prev = node2;
+	print_list(*list);
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list in ascending order
+ * @list: pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *tmp;
-	if (list == NULL || *list == NULL)
+	listint_t *curr = (*list)->next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	current = (*list)->next;
-	while (current != NULL)
+	while (curr != NULL)
 	{
-		while (current->prev != NULL && current->n < current->prev->n)
-		{
-			tmp = current->prev;
-			tmp->next = current->next;
+		listint_t *prev = curr->prev;
 
-			if (tmp->prev != NULL)
-				tmp->prev->next = current;
-			if (current->next != NULL)
-				current->next->prev = tmp;
-			current->prev = tmp->next;
-			tmp->prev = current;
-			current->next = tmp;
-			if (current->prev == NULL)
-				*list = current;
+		while (prev != NULL && prev->n > curr->n)
+		{
+			swap_nodes(list, prev, curr);
+			prev = curr->prev;
 		}
-		current = current->next;
+		curr = curr->next;
 	}
 }
