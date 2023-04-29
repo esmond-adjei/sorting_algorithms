a48afd8c0ee514e6a07000f8de7af0af777fa6f4
esmond-adjei <xmongram@gmail.com>
2023-04-29 11:21:36 +0000

    modified some files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,96 +1,46 @@
 #include "sort.h"
 
 /**
- * swap_left -swap two nodes right left position
- * @list: list
- *Return: NULL
- *
- **/
-void swap_left(listint_t *list)
+ * swap_nodes - swaps nodes in a doubly linked list
+ * @list: pointer to the head of the list
+ * @node1: first node
+ * @node2: second node
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-	listint_t *tmp, *head;
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	else
+		*list = node2;
 
-	while (list->prev != NULL)
-	{
-		if (list->n < list->prev->n)
-		{
-			tmp = list->prev->prev;
-			list->prev->next = list->next;
-			list->next = list->prev;
-			list->prev->prev = list;
-			list->prev = tmp;
-			list->next->next->prev = list->next;
-			if (tmp != NULL)
-				tmp->next = list;
-			head = list;
-			while (head->prev != NULL)
-				head = head->prev;
-			print_list(head);
-		}
-		else
-			list = list->prev;
-	}
+	if (node2->next != NULL)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node2->next = node1;
+	node1->prev = node2;
+	print_list(*list);
 }
 
 /**
- * swap_right -swap two nodes left rigth position
- * @l: list
- * Return: Null
- **/
-void swap_right(listint_t *l)
-{
-	listint_t *tmp, *head;
-
-	tmp = l->prev;
-
-	if (tmp != NULL)
-	{
-		tmp->next = l->next;
-		l->next->prev = tmp;
-	}
-	else
-		l->next->prev = NULL;
-	l->prev = l->next;
-	if (l->next->next != NULL)
-	{
-		l->next = l->next->next;
-		l->prev->next = l;
-		l->next->prev = l;
-	}
-	else
-	{
-		l->next->next = l;
-		l->next = NULL;
-	}
-	head = l;
-	while (head->prev != NULL)
-		head = head->prev;
-	print_list(head);
-	swap_left(l->prev);
-}
-
-/**
- * insertion_sort_list -sorts a doubly linked list in ascending order
- * @list: list
- * Return: NULL
+ * insertion_sort_list - sorts a doubly linked list in ascending order
+ * @list: pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *l;
-
-	if ((list == NULL) || (*list == NULL) || ((*list)->next == NULL))
+	listint_t *curr = (*list)->next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	l = *list;
 
-	while (l->next != NULL)
+	while (curr != NULL)
 	{
-		if (l->n > l->next->n)
+		listint_t *prev = curr->prev;
+		while (prev != NULL && prev->n > curr->n)
 		{
-			swap_right(l);
+			swap_nodes(list, prev, curr);
+			prev = curr->prev;
 		}
-		else
-			l = l->next;
+		curr = curr->next;
 	}
-	while ((*list)->prev != NULL)
-		*list = (*list)->prev;
 }
